d2a182d9d8532fe433b2527c9582ad2e9f729dc1
Jatzuk <jatzuk.22@gmail.com>
2019-01-24 21:31:57 +0300

    start implementing horizontal search for adding new obstacles + tests

--- src/main/kotlin/tetrosnake/Obstacle.kt
+++ src/main/kotlin/tetrosnake/Obstacle.kt
@@ -2,7 +2,6 @@ package tetrosnake
 
 import tetrosnake.Canvas.Companion.OBSTACLE_TAG
 import tetrosnake.Canvas.Companion.board
-import tetrosnake.Canvas.Companion.arrangeNewGameObject
 import util.Util.randomX
 import util.Util.randomY
 import java.util.*
@@ -22,43 +21,74 @@ import java.util.*
  */
 
 class Obstacle(size: Int = Canvas.WALL_SIZE, snake: Snake? = null) {
-    init {
-        if (snake == null) {
-//            while (true) {
-//                val x = randomX(3)
-//                val y = randomY(3)
-//                if (Random().nextBoolean()) {
-//                    if (arrangeNewGameObject(x, y, x + 3, y)) {
-//                        for (i in 0 until size) {
-//                            body.add(Point(x + i, y))
-//                            board[y][x + i] = OBSTACLE_TAG
-//                        }
-//                        break
-//                    } else continue
-//                } else {
-//                    if (arrangeNewGameObject(x, y, x, y + 3)) {
-//                        for (i in 0 until size) {
-//                            body.add(Point(x, y - i))
-//                            board[y - i][x] = OBSTACLE_TAG
-//                        }
-//                        break
-//                    } else continue
-//                }
-//            }
+//    init {
+//        if (snake == null) {
+//            val directionFlag = Random().nextBoolean()
+//            val (x, y) = arrangeObstacle(randomX(2), randomY(2), true)!!
+//            if (true) {
+//                for (i in 0 until size) board[y][x + i] = OBSTACLE_TAG
+//            } else for (i in 0 until size) board[y - i][x] = OBSTACLE_TAG
+//
+//        } else {
+//            for (i in 0 until snake.body.size) board[snake.body[i].y][snake.body[i].x] = OBSTACLE_TAG
+//
+////            {
+////                body.add(Point(snake.body[i]))
+//
+//        }
+//    }
 
+    fun arrangeObstacle(x: Int, y: Int, direction: Boolean): Pair<Int, Int>? {
+        var pair: Pair<Int, Int>? = null
 
-            val directionFlag = Random().nextBoolean()
-            val (x, y) = arrangeNewGameObject(randomX(2), randomY(2), true, size)!!
-            if (true) {
-                for (i in 0 until size) board[y][x + i] = OBSTACLE_TAG
-            } else for (i in 0 until size) board[y - i][x] = OBSTACLE_TAG
-
+        if (direction) {
+            pair = horizontalSearch(x, y)
+            if (pair == null) {
+                println("did not found any solutions on $y changing axis...")
+                pair = changeYAxis(x, y)
+            }
         } else {
-            for (i in 0 until snake.body.size) board[snake.body[i].y][snake.body[i].x] = OBSTACLE_TAG
-
-//            {
-//                body.add(Point(snake.body[i]))
 
         }
+
+        return pair
+    }
+
+    private fun horizontalSearch(x: Int, y: Int, recLvl: Int = 0, range: IntRange = -1..-1): Pair<Int, Int>? {
+        var pair: Pair<Int, Int>? = null
+        if (x - 1 in range || x < 1 || x > Canvas.WIDTH - 1) {
+            println("\t\tcan not add for line $y, returning null")
+            return pair
+        }
+
+        val xl = x - 1
+        val xr = x + 1
+        println("trying to add object, x: $x, y: $y, reclvl: $recLvl")
+
+        if (board[y][xl] == Canvas.EMPTY_TAG && board[y][x] == Canvas.EMPTY_TAG && board[y][xr] == Canvas.EMPTY_TAG) {
+            println("\t\tmatched at x: $x, y: $y | $xl..$xr, reclvl: $recLvl")
+            pair = Pair(xl, y)
+        } else {
+            if (pair == null && x / 2 >= 1/* && x / 2 !in range*/) {
+                println("\tleft-> ${xl - 1} >=")
+                pair = horizontalSearch(x / 2, y, recLvl + 1, xl..xr)
+            }
+            if (pair == null && x * 2 <= Canvas.WIDTH - 1) {
+                println("\tright-> ${xr + 1} <= ${Canvas.WIDTH - 1}")
+                pair = horizontalSearch(x * 2, y, recLvl + 1, xl..xr)
+            }
+        }
+        return pair
+    }
+
+    private fun changeYAxis(x: Int, y: Int, recLvl: Int = 0): Pair<Int, Int>? {
+        var pair: Pair<Int, Int>? = null
+
+        if (y < 0 || y > Canvas.HEIGHT - 1) return pair
+
+        while (y > -1 && pair == null) pair = horizontalSearch(x, y / 2, recLvl + 1)
+        while (y > Canvas.HEIGHT - 1 && pair == null) pair = horizontalSearch(x, y * 2, recLvl + 1)
+
+        return pair
     }
 }
